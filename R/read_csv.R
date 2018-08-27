a58a122e928eecab3c7b5b98d0670ddc2ea1e9b2
Nandhini31294 <42430670+Nandhini31294@users.noreply.github.com>
2018-08-27 11:40:41 +0530

    first commit

---
+++ R/read_csv.R
@@ -0,0 +1,15 @@
+#' A read_csv function
+#'
+#' This function allows you to read a csv file
+#' @param filename has the filepath
+#' @return success/failure
+#' @export
+
+read_csv<-function(filename)
+{
+  x<-read.csv(filename)
+  return("Read successfully")
+}
+
+
+
